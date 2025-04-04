0f84bfeffc799bc0ce086120137185a9abbfae1f
olsenola <202180203@psu.palawan.edu.ph>
2025-04-05 00:33:46 +0800

    Modified views.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
+from django.views.generic.list import ListView # type: ignore
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
 
 # Create your views here.
